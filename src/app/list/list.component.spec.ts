e1316e5f13e280ee28134f9b3f884edd5c62635a
raphael agostini <raphael.agostini@ynov.com>
2020-02-22 15:56:35 +0100

    add modules for list unit test

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListComponent } from './list.component';
+import {MatPaginatorModule, MatTableModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('ListComponent', () => {
   let component: ListComponent;
@@ -8,6 +11,7 @@ describe('ListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatTableModule, MatPaginatorModule, HttpClientModule, BrowserAnimationsModule],
       declarations: [ ListComponent ]
     })
     .compileComponents();
